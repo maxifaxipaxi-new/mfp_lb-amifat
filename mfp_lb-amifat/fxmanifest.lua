44149815e00f2ac2e01fd41da8f8994a3954da76
MaxiFaxiPaxi <115405418+maxifaxipaxi-new@users.noreply.github.com>
2025-03-11 09:58:45 +0100

    Add files via upload

---
+++ mfp_lb-amifat/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version "cerulean"
+game "gta5"
+
+title "LB-Phone - Am I Fat -App"
+description "Integrate Apps like Am I Fat to LB-Phone"
+author "MFPSCRIPTS"
+version '1.0'
+
+lua54 'yes'
+
+client_script {
+    'client.lua',
+    'config.lua'
+}
+
+files {
+    "ui/**/*"
+}
+
+ui_page "ui/index.html"
+
+-- visit lb-phone.com for phone! --
+-- visit lb.mfpscripts.com for addons! --
